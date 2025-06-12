af8b7f569c8fbad11881e746f32d10e2ee775a3e
jpassano <jorgepassano@gmail.com>
2025-06-11 22:18:29 -0600

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from 'react';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import Alert from './components/Alert';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [jwtToken, setJwtToken] = useState('');
+    const [alertMessage, setAlertMessage] = useState();
+    const [alertClassName, setAlertClassName] = useState('d-none');
+
+    const [tickInterval, setTickInterval] = useState();
+
+    const navigate = useNavigate();
+
+    const logOut = () => {
+        const requestOptions = {
+            method: "GET",
+            credentials: "include",
+        }
+
+        fetch(`${process.env.REACT_APP_BACKEND}/logout`, requestOptions)
+        .catch(error => {
+            console.log("error loging out", error);
+        })
+        .finally(() => {
+            setJwtToken("");
+            toggleRefresh(false);
+        })
+
+        navigate("/login");
+    }
+
+    const toggleRefresh = useCallback((status) => {
+        console.log("clicked");
+
+        if (status ) {
+            console.log("turning on ticking");
+            let i = setInterval(() => {
+                const requestOptions = {
+                    method: "GET",
+                    credentials: "include",
+                }
+    
+            fetch(`${process.env.REACT_APP_BACKEND}/refresh`, requestOptions)
+                .then((response) => response.json())
+                .then((data) => {
+                    if(data.access_token) {
+                        setJwtToken(data.access_token);
+                    }
+                })
+                .catch(error => {
+                    console.log("user is not logged in");
+                })
+            }, 600000);
+            setTickInterval(i);
+            console.log("setting tick interval to",  i);
+        } else {
+            console.log("turning off ticking");
+            console.log("turning off tickinterval", tickInterval);
+            setTickInterval(null);
+            clearInterval(tickInterval);
+        }
+    }, [tickInterval])
+
+
+    useEffect(() => {
+        if (jwtToken ==="") {
+            const requestOptions = {
+                method: "GET",
+                credentials: "include",
+            }
+            fetch(`${process.env.REACT_APP_BACKEND}/refresh`, requestOptions)
+                .then((response) => response.json())
+                .then((data) => {
+                    if(data.access_token) {
+                        setJwtToken(data.access_token);
+                        toggleRefresh(true);
+                    }
+                })
+                .catch(error => {
+                    console.log("user is not logged in", error);
+                })
+        }
+    }, [jwtToken, toggleRefresh])
+
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1 className="mt-3">Server Authentication for All the Apps!</h1>
+                </div>
+                <div className="col text-end">
+                    {jwtToken === '' 
+                        ? ( <Link to='login'><span className="badge bg-success">Login</span></Link> )
+                        : (<a href='#!' onClick={logOut}><span className='badge bg-danger'>Logout</span></a>)
+                    }
+                </div>
+                <hr className="mb-3"></hr>
+            </div>
+
+            <div className="row">
+                <div className="col-md-2">
+                    <nav>
+                        <div className='list-group'>
+                            <Link to='/' className="list-group-item list-group-item-action">Home</Link>
+                            {jwtToken !== '' && (
+                                <>
+                                    <Link to='/apps' className="list-group-item list-group-item-action">Apps</Link>
+                                    <Link to='/databases' className="list-group-item list-group-item-action">Databases</Link>
+                                    <Link to='/roles' className="list-group-item list-group-item-action">Roles</Link>
+                                    <Link to='/users' className="list-group-item list-group-item-action">Users</Link>
+                                    <Link to='/groups' className="list-group-item list-group-item-action">Groups</Link>
+                                    <Link to='/profiles' className="list-group-item list-group-item-action">Profiles</Link>
+                                    <Link to='/ips' className="list-group-item list-group-item-action">IPs</Link>
+                                    <Link to='/regions' className="list-group-item list-group-item-action">Regions</Link>
+                                    <Link to='/biometrics' className="list-group-item list-group-item-action">Biometrics</Link>
+                                    {/* <Link to='/manage-catalogue' className="list-group-item list-group-item-action">Manage Catalogue</Link> */}
+                                </> 
+                            )}
+                        </div>
+                    </nav>
+                </div>
+                <div className="col-md-10 ">
+                    <Alert
+                        message={alertMessage}
+                        className={alertClassName}
+                    />
+                    <Outlet context={{
+                        jwtToken, 
+                        setJwtToken,
+                        setAlertClassName,
+                        setAlertMessage,
+                        toggleRefresh,
+                    }} />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import Apps from './components/Apps';
+import Databases from './components/Databases';
+import Roles from './components/Roles';
+import Users from './components/Users';
+import Groups from './components/Groups';
+import Profiles from './components/Profiles';
+import IPs from './components/IPs';
+import Regions from './components/Regions';
+import Biometrics from './components/Biometrics';
+import Login from './components/Login';
+// import Logout from './components/Logout';
+import Thisapp from './components/ThisApp';
+// import ManageCatalogue from './components/ManageCatalogue';
+import EditApp from './components/EditApp';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <Home /> },
+      {
+        path: "/apps",
+        element: <Apps />,
+      },
+      {
+        path: "/apps/:id",
+        element: <Thisapp />,
+      },
+      {
+        path: "/databases",
+        element: <Databases />,
+      },
+      {
+        path: "/roles",
+        element: <Roles />,
+      },
+      {
+        path: "/users",
+        element: <Users />,
+      },
+      {
+        path: "/groups",
+        element: <Groups />,
+      },
+      {
+        path: "/profiles",
+        element: <Profiles />,
+      },
+      {
+        path: "/ips",
+        element: <IPs />,
+      },
+      {
+        path: "/regions",
+        element: <Regions />,
+      },
+      {
+        path: "/biometrics",
+        element: <Biometrics />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      // {
+      //   path: "/logout",
+      //   element: <Logout />,
+      // },
+      // {
+      //   path: "/manage-catalogue",
+      //   element: <ManageCatalogue />,
+      // },
+      {
+        path: "/thisapp/:id",
+        element: <EditApp />,
+      },
+      {
+        path: "/admin/thisapp/0",
+        element: <EditApp />,
+      },
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
